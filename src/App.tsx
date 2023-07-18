b0cdc9545016870d30110e9ac63768098117d1dd
Furqan Abid <thisismefurqan@gmail.com>
2023-07-18 11:08:39 +0500

    Add alert component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import ListGroup from './components/ListGroup';
 function app() {
   let items = ["New York", "Paris", "London", "Buenos Aires", "San Jose"];
 
+  const handleSelectItem = (item : string) => {
+    console.log(item)
+  }
   return(
    <div>
-      <ListGroup items = {items} heading='Cities'/>
+      <ListGroup items = {items} heading='Cities' onSelectItem={handleSelectItem}/>
     </div>
   );}
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,10 +2,11 @@ import { Fragment, useState } from "react";
 interface Props{
     items: string[];
     heading: string;
+    onSelectItem: (item: string) => void;
 }
 
 
-function ListGroup({items, heading}: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1)
   
 
@@ -18,7 +19,9 @@ function ListGroup({items, heading}: Props) {
         <li 
         className = { selectedIndex === index ? "list-group-item active" : "list-group-item"}
         key={item}
-        onClick = {() => {setSelectedIndex(index);}}
+        onClick = {() => {setSelectedIndex(index);
+        onSelectItem(item);
+        }}
         >{item}</li>
 
       ))}
