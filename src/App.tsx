818ebf798cf2b3f29ac1c8a773715c86658f8aa0
Furqan Abid <thisismefurqan@gmail.com>
2023-07-18 13:03:08 +0500

    Add alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Alert from './components/Alert';
 import ListGroup from './components/ListGroup';
 import Button from './components/Button';
+import { useState } from 'react';
 
 function app() {
+  const [alertVisible, setAlertVisibility] = useState(false)
+
   return (
-    <Button /*color='secondary'*/ onClick={() => console.log('Clicked !')}>
-      Click Me
-    </Button>
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>You clicked</Alert>}
+      <Button /*color='secondary'*/ onClick={() => setAlertVisibility(true)}>
+        Click Me
+      </Button>
+    </div>
   );
 }
 
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,11 +2,17 @@ import { ReactNode } from "react";
 
 interface Props{
     children:ReactNode;
+    onClose : () => void;
 }
 
-const Alert = ({children}:Props) => {
+const Alert = ({children, onClose} : Props) => {
   return (
-    <div>{children}</div>
+    <div className="alert alert-primary alert-dismissible">
+      {children}
+      <button type="button" className="close" onClick={onClose} data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
   )
 }
 
