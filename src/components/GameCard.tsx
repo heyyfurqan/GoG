3fa16df14376031108cd6cf9615ad6f12645fcc8
Furqan Abid <thisismefurqan@gmail.com>
2023-07-24 10:44:24 +0500

    Add game-loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,17 @@
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
-
+import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image} />
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
@@ -24,5 +24,4 @@ const GameCard = ({ game }: Props) => {
     </Card>
   );
 };
-
 export default GameCard;
